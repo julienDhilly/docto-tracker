2cdb8197480b7782c8e31b7a0954715ed7211a0a
Julien Needone <julien.dhilly@needone.fr>
2021-05-19 13:16:05 +0000

    add eslint and format sources

--- src/lib/notifier.js
+++ src/lib/notifier.js
@@ -1,34 +1,36 @@
-import Vonage from "@vonage/server-sdk";
+/* eslint-disable no-console */
+/* eslint-disable import/prefer-default-export */
+import Vonage from '@vonage/server-sdk';
 import TinyURL from 'tinyurl';
 
 const vonage = new Vonage({
-   apiKey: process.env.vonageApiKey,
-   apiSecret: process.env.vonageApiSecret
-})
+  apiKey: process.env.vonageApiKey,
+  apiSecret: process.env.vonageApiSecret,
+});
 
-export function sendAlert(result, phoneNumbers) {
-   const url = `https://www.doctolib.fr${result.url}`;
-   TinyURL.shorten(url, function(res, err) {
-      if (err) {
-         console.log(err);
-      } else {
-         const from = "DocTracker"
-         const text = `A slot has been found at ${res}`;
-         phoneNumbers.forEach(to => sendSMS({from, to, text}));
-      }
-   });
+function sendSMS({ from, to, text }) {
+  vonage.message.sendSms(from, to, text, (err, responseData) => {
+    if (err) {
+      console.log(err);
+    } else if (responseData.messages[0].status === '0') {
+      console.log('Message sent successfully.');
+    } else {
+      console.log(
+        `Message failed with error: ${responseData.messages[0]['error-text']}`,
+      );
+    }
+  });
 }
 
-function sendSMS({from, to, text}) {
-   vonage.message.sendSms(from, to, text, (err, responseData) => {
-      if (err) {
-         console.log(err);
-      } else {
-         if(responseData.messages[0]['status'] === "0") {
-               console.log("Message sent successfully.");
-         } else {
-               console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
-         }
-      }
-   });
+export function sendAlert(result, phoneNumbers) {
+  const url = `https://www.doctolib.fr${result.url}`;
+  TinyURL.shorten(url, (res, err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const from = 'DocTracker';
+      const text = `A slot has been found at ${res}`;
+      phoneNumbers.forEach((to) => sendSMS({ from, to, text }));
+    }
+  });
 }
--- src/main.js
+++ src/main.js
@@ -1,81 +1,90 @@
+/* eslint-disable no-console */
 import config from 'config';
 import Crawler from 'crawler';
-import {sendAlert} from './lib/notifier.js';
+import { sendAlert } from './lib/notifier';
 
-// stop the script when a slot found. TODO: Check if slot alert as already been send and keep crawling
+// stop the script when a slot found.
+// TODO: Check if slot alert as already been send and keep crawling
 let find = false;
 
 const c = new Crawler({
-    maxConnections : 6,
-    rateLimit: 1000,
-    jQuery: false,
-    // This will be called for each crawled page
-    callback: (error, res, done) => {
-        if(error){
-            console.log(error);
-        }else{
-           const result = JSON.parse(res.body);
-           try {
-            const allowed = config.get('allowedCities').find(str => result.search_result.url.includes(`/${str}/`));
-            if (!allowed) {
-               console.log(result.search_result.id, 'ignore');
-               done();
-               return;
-            }
-            if (result.total > 0) {
-                find = true;
-                console.log(result);
-                console.log(result.search_result.id, 'YEAH', result.search_result.url);
-                sendAlert(result.search_result, config.get('phoneNumbers'));
-            } else {
-                console.log(result.search_result.id, 'No slot found');
-            }
-           }catch(err) {
-               console.error(err);
-           }
+  maxConnections: 6,
+  rateLimit: 1000,
+  jQuery: false,
+  // This will be called for each crawled page
+  callback: (error, res, done) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const result = JSON.parse(res.body);
+      try {
+        const allowed = config
+          .get('allowedCities')
+          .find((str) => result.search_result.url.includes(`/${str}/`));
+        if (!allowed) {
+          console.log(result.search_result.id, 'ignore');
+          done();
+          return;
         }
-        done();
+        if (result.total > 0) {
+          find = true;
+          console.log(result);
+          console.log(
+            result.search_result.id,
+            'YEAH',
+            result.search_result.url,
+          );
+          sendAlert(result.search_result, config.get('phoneNumbers'));
+        } else {
+          console.log(result.search_result.id, 'No slot found');
+        }
+      } catch (err) {
+        console.error(err);
+      }
     }
+    done();
+  },
 });
 
-start();
-
-c.on('drain',function(){
-   if (!find) {
-      start();
-   }
-});
-
-function start() {
-   c.queue({
-      uri:'https://www.doctolib.fr/vaccination-covid-19/rennes?ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005&force_max_limit=2',
-      callback: (error, res, done) => {
-         if(error){
-             console.log(error);
-         }else{
-            const ids = parseIds(res.body);
-            console.log('-------------------------');
-            console.log('SET OF IDS', JSON.stringify(ids));
-            addQueues(ids);
-         }
-
-         done();
-     }
-   });
+function addQueues(ids) {
+  c.queue(
+    ids.map((id) => `https://www.doctolib.fr/search_results/${id}.json?ref_visit_motive_ids%5B%5D=6970&ref_visit_motive_ids%5B%5D=7005&speciality_id=5494&search_result_format=json&force_max_limit=2`),
+  );
 }
 
 function parseIds(page) {
-   const ids = [];
-   const regex = /data-props="{&quot;searchResultId&quot;:([0-9]+),&quot;/gm;
-   let regResult;
-   while ((regResult = regex.exec(page)) !== null) {
-      ids.push(regResult[1]);
-   }
-   return ids;
+  const ids = [];
+  const regex = /data-props="{&quot;searchResultId&quot;:([0-9]+),&quot;/gm;
+  let regResult = null;
+  // eslint-disable-next-line no-cond-assign
+  while (regResult = regex.exec(page) !== null) {
+    ids.push(regResult[1]);
+  }
+  return ids;
 }
 
-function addQueues(ids) {
-   c.queue(ids.map(id => {
-      return `https://www.doctolib.fr/search_results/${id}.json?ref_visit_motive_ids%5B%5D=6970&ref_visit_motive_ids%5B%5D=7005&speciality_id=5494&search_result_format=json&force_max_limit=2`;
-   }));
+function start() {
+  c.queue({
+    uri: 'https://www.doctolib.fr/vaccination-covid-19/rennes?ref_visit_motive_ids[]=6970&ref_visit_motive_ids[]=7005&force_max_limit=2',
+    callback: (error, res, done) => {
+      if (error) {
+        console.log(error);
+      } else {
+        const ids = parseIds(res.body);
+        console.log('-------------------------');
+        console.log('SET OF IDS', JSON.stringify(ids));
+        addQueues(ids);
+      }
+
+      done();
+    },
+  });
 }
+
+start();
+
+c.on('drain', () => {
+  if (!find) {
+    start();
+  }
+});
